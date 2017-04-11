b9169f3a66205c3915b7946cb47776848e3f8cca
Michael Ries <michael.ries@gmail.com>
2017-04-11 16:25:34 +0000

    show_game_details now accepts an argument corresponding to the user choice" git st

--- lib/todays_baseball/cli.rb
+++ lib/todays_baseball/cli.rb
@@ -1,6 +1,5 @@
 class TodaysBaseball::CLI
 
-
   def start
     list_games
     get_input
@@ -9,11 +8,6 @@ class TodaysBaseball::CLI
   def list_games
     puts "Today's Baseball Games"
     puts "----------------------"
-    # puts "1. Mets @ Phillies, 3 to 4, Final"
-    # puts "2. Carinals @ Cubs, 2 to 0, Top of the 4th"
-    # puts "3. Reds @ Pirates, 2:05 ET"
-    # puts "4. Astros @ Seahawks, Delayed"
-    # puts ""
     @@games = TodaysBaseball::Game.games
     @@games.each.with_index do |game, item|
       puts "#{item+1}. #{game.visitor_team_name} @ #{game.home_team_name}, #{game.score}, #{game.status}"
@@ -27,13 +21,14 @@ class TodaysBaseball::CLI
       if input == 'list'
         list_games
       elsif input.to_i>0
-        show_game_details
+        show_game_details(input.to_i)
       else
         puts "I don't recognize that command.  Please try again."
       end
     end
   end
 
-  def show_game_details
+  def show_game_details(game_number)
+    puts "Details for game #{game_number}."
   end
 end
