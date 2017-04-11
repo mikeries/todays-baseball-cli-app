2bf97c0845b2e7c202d4e1d81d9cdd9f5be4256f
Michael Ries <michael.ries@gmail.com>
2017-04-11 18:38:16 +0000

    add scraper.rb, add nokogiri and open-uri to environment

--- lib/todays_baseball.rb
+++ lib/todays_baseball.rb
@@ -1,6 +1,9 @@
+require 'nokogiri'
+require 'open-uri'
 require_relative "./todays_baseball/version"
 require_relative "./todays_baseball/team"
 require_relative "./todays_baseball/player"
 require_relative "./todays_baseball/pitcher"
 require_relative "./todays_baseball/game"
+require_relative "./todays_baseball/scraper"
 require_relative "./todays_baseball/cli"
---
+++ lib/todays_baseball/scraper.rb
@@ -0,0 +1,7 @@
+require 'nokogiri'
+require 'open-uri'
+
+class TodaysBaseball::Scraper
+  def self.scrape_games
+  end
+end
